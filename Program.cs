eca8c572a786b5ff1ecc91005b2845323db48111
Tamer <tamer.masud@mail.ru>
2024-02-05 13:13:41 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace FirstApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte Age = 25;
+            string Name = "Tamer";
+            bool Pet = true;
+            double foot = 42.5;
+
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My age " + Age);
+            Console.WriteLine("Do i have a pet: " + Pet);
+            Console.WriteLine("My shoe size is " + foot);
+
+        }
+    }
+}
